8dc6206fee1fc80bb8f4fa80fe34e02e3288b718
Joseph Benoy <josephbenoy03@gmail.com>
2021-05-24 00:28:32 +0530

    modiefed mehods

--- classes/bot.class.php
+++ classes/bot.class.php
@@ -1,16 +1,8 @@
 <?php
     namespace Telegram\Api;
-/**
- * [Base class for the bot sdk]
- */
     class Bot{
         protected $apiToken;
         protected $classArray=[];
-        /**
-         * Pass api token. Optionally can pass the command class array
-         * @param mixed $token api token
-         * @param mixed $classArray=null command class array
-         */
         public function __construct($token,$classArray=null){
             $this->apiToken = $token;
             if($classArray!=null){
@@ -19,12 +11,14 @@
                 }
             }
         }
-                /**
-         * filter the update object and return filter object to identify the type
-         * @param mixed $updateObj
-         * 
-         * @return stdClass
-         */
+        protected function isCommand($text){
+            if(substr($text,0,1)=="/"){
+                return true;
+            }
+            else{
+                return false;
+            }
+        }
         protected function filterUpdate($updateObj):\stdClass{
             if(property_exists($updateObj,'callback_query')){
                 $filterObj = new \stdClass;
@@ -32,11 +26,19 @@
                 $filterObj->queryData = $updateObj->callback_query->data;
                 return $filterObj;
             }
-            elseif($cmd = $this->getCommandName($updateObj)!=null){
-                $filterObj = new \stdClass;
-                $filterObj->chatId = $updateObj->message->chat->id;
-                $filterObj->commandName = $cmd;
-                return $filterObj;
+            elseif($this->isCommand($updateObj->message->text)){
+                if($cmd = $this->getCommandName($updateObj->message->text)!=null){
+                    $filterObj = new \stdClass;
+                    $filterObj->chatId = $updateObj->message->chat->id;
+                    $filterObj->commandName = $cmd;
+                    return $filterObj;
+                }
+                else{
+                    $filterObj = new \stdClass;
+                    $filterObj->chatId = $updateObj->message->chat->id;
+                    $filterObj->commandName = "";
+                    return $filterObj;
+                }
             }
             else{
                 $filterObj = new \StdClass;
@@ -44,14 +46,8 @@
                 return $filterObj;
             }
         }
-        /**
-         * Get filter out command name from update object . Returns null if the command is not registeres
-         * @param mixed $updateObj
-         * 
-         * @return [type]
-         */
-        protected function getCommandName($updateObj){
-            $data = ltrim($updateObj->message->text,"/");
+        protected function getCommandName($text){
+            $data = ltrim($text,"/");
             if(in_array($data,$this->classArray)){
                 return $data;
             }
@@ -59,10 +55,6 @@
                 return null;
             }
         }
-        /**
-         * capture the update object sent to webhook
-         * @return void
-         */
         public function capture():void{
             $updateObj = json_decode(file_get_contents("php://input"));
             $filterObj = $this->filterUpdate($updateObj);
@@ -71,13 +63,18 @@
             }
             elseif(property_exists($filterObj,'commandName')){
                 //execute as registered command
-                $this->executeCommand($filterObj,$updateObj);
+                if($filterObj->commandName==""){
+                    $this->sendError($filterObj,"Invalid Command");// send error message because command is not registered
+                }
+                else{
+                    $this->executeCommand($filterObj,$updateObj);
+                }
             }
             else{
                 //execute as random input
             }
         }
-        protected function executeCommand($filterObj,$updateObj){
+        protected function executeCommand($filterObj,$updateObj):void{
             $command = new $filterObj->commandName($updateObj);
             $command->handle();
         }
