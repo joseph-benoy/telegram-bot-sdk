a2142f3729a93bb8f67a1d1ce581f792cf5a0417
Joseph Benoy <josephbenoy03@gmail.com>
2021-05-24 01:21:09 +0530

    changes to command class

--- classes/command.class.php
+++ classes/command.class.php
@@ -1,6 +1,6 @@
 <?php
     namespace Telegram\Api;
-    abstract class Command extends \Telegram\Api\Actions\ReplyActions{
+    abstract class Command extends \Telegram\Api\Actions\ReplyAction{
         protected $updateObj;
         protected $apiToken;
         public function __construct($apiToken,$updateObj){
@@ -19,6 +19,16 @@
         }
         public function getFullname(){
             return  $this->updateObj->from->first_name." ".$this->updateObj->from->last_name;
+        }
+        protected sendReply($method,$data){
+            $url = "https://api.telegram.org/bot".$this->apiToken. "/" . $method;
+            $curl = curl_init($url);
+            curl_setopt($curl, CURLOPT_POST, true);
+            curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+            curl_setopt($curld, CURLOPT_RETURNTRANSFER, true);
+            $output = curl_exec($curl);
+            curl_close($curl);
+            return $output;
         }
     }
 ?>
