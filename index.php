0311924936323dd03d5d7aa756cf1a178b6fb70e
Joseph Benoy <josephbenoy03@gmail.com>
2021-05-24 01:48:29 +0530

    chnages to classes

--- index.php
+++ index.php
@@ -1,5 +1,11 @@
 <?php
     require_once("config.php");
+    class start extends Telegram\Api\Command{
+        public function handle(){
+            $this->sendReply("sendMessage",array(""=>"","text"=>"Welcome to the bot!"));
+        }
+    }
     $bot = new \Telegram\Api\Bot('1755386616:AAFH3PIzoumgJn1nOEy-i_YV8evDUUWq0qk');
+    $bot->registerCommands(['start']);
     $bot->capture();
 ?>
