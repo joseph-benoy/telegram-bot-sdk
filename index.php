f9432e5dfdaf67717a502dd781040ce975016fe3
Joseph Benoy <josephbenoy03@gmail.com>
2021-05-24 02:09:42 +0530

    changes

--- index.php
+++ index.php
@@ -2,10 +2,15 @@
     require_once("config.php");
     class start extends Telegram\Api\Command{
         public function handle(){
-            $this->sendReply("sendMessage",array(""=>"","text"=>"Welcome to the bot!"));
+            $this->sendReply("sendMessage",array("text"=>"Welcome to the bot!"));
+        }
+    }
+    class ping extends Telegram\Api\Command{
+        public function handle(){
+            $this->sendReply("sendMessage",array("text"=>"Oombikko!!"));
         }
     }
     $bot = new \Telegram\Api\Bot('1755386616:AAFH3PIzoumgJn1nOEy-i_YV8evDUUWq0qk');
-    $bot->registerCommands(['start']);
+    $bot->registerCommands(['start','ping']);
     $bot->capture();
 ?>
