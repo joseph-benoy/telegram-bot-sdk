e81cbcffd7dc9ca554de602e72080a724282c379
Joseph Benoy <josephbenoy03@gmail.com>
2021-05-25 22:04:33 +0530

    added docs

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <?php
-    require_once("autoload.php");
+    require_once("autoload.php"); //autoload all classes required
+/**
+ * [Start command with handle method defintion]
+ */
     class start extends Telegram\Api\Command{
         public function handle(){
             $this->replyMessage("Welcome to the bot!");
         }
     }
-    $bot = new \Telegram\Api\Bot('1755386616:AAFH3PIzoumgJn1nOEy-i_YV8evDUUWq0qk');
-    $bot->registerCommands(['start','ping']);
-    $bot->capture();
+    $bot = new \Telegram\Api\Bot('bot_token'); //enter your bot token here
+    $bot->registerCommands(['start','ping']); //register the command to the bot handler class
+    $bot->capture(); //capture the commands invoked by the client
 ?>
