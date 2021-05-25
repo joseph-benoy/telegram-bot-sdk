500ffb65222b61e2021e0598eb879d7112605557
Joseph Benoy <josephbenoy03@gmail.com>
2021-05-25 22:00:32 +0530

    added sendmessage function using chatId explicitly

--- index.php
+++ index.php
@@ -5,19 +5,6 @@
             $this->replyMessage("Welcome to the bot!");
         }
     }
-    class ping extends Telegram\Api\Command{
-        public function handle($randomMessage=null,$commandSessionObj=null,$queryData=null){
-
-            $obj = new Telegram\component\InlineKeyboard();
-            $obj->addRow([["text"=>"@@@@","callback_data"=>"@@@@"],["text"=>"google","callback_data"=>"######"]]);
-            $result = $this->replyVideo("20210525_150149.mp4","light-sign-typography-lighting.jpg","caption",$obj->getMarkup());
-        
-        
-        
-        
-        
-        }
-    }
     $bot = new \Telegram\Api\Bot('1755386616:AAFH3PIzoumgJn1nOEy-i_YV8evDUUWq0qk');
     $bot->registerCommands(['start','ping']);
     $bot->capture();
